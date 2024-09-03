87d31a37a7a19c8f9fe99c1a9833b93d633134cc
Jochem Loedeman <j.m.loedeman@gmail.com>
2024-09-03 23:08:33 +0200

    Update images

--- scripts/distribute_images.js
+++ scripts/distribute_images.js
@@ -1,21 +1,25 @@
 // Array of image sources
 const images = [
-    "/assets/images/P1070346-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070333-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070384-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070378-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070347-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070311-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070358-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070355-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070351-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070365-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070381-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070396-Enhanced-RD Large.jpeg",
-    "/assets/images/P1070303-Enhanced-RD Large.jpeg"
+    '/assets/images/P1070378-Enhanced-RD Large.jpeg',
+    '/assets/images/DSCF4429-Enhanced-RD Large.jpeg',
+    '/assets/images/P1070333-Enhanced-RD Large.jpeg',
+    '/assets/images/DSCF4744-Enhanced-RD Large.jpeg',
+    '/assets/images/P1070384-Enhanced-RD Large.jpeg',
+    '/assets/images/DSC09328-Enhanced-NR Large.jpeg',
+    '/assets/images/P1070444 Large.jpeg',
+    '/assets/images/DSCF4570-Enhanced-RD-2 Large.jpeg',
+    '/assets/images/P1070347-Enhanced-RD Large.jpeg',
+    '/assets/images/DSCF4602-Enhanced-RD-2 Large.jpeg',
+    '/assets/images/P1070303-Enhanced-RD Large.jpeg',
+    '/assets/images/DSC08987 Large.jpeg',
+    '/assets/images/DSC08870 Large.jpeg',
+    '/assets/images/DSC09061 Large.jpeg',
+    '/assets/images/DSCF4667-Enhanced-RD Large.jpeg',
+    '/assets/images/DSCF4310-Enhanced-RD Large.jpeg',
 ];
 
 const twoColumnThreshold = 768;
+
 const threeColumnThreshold = 1024;
 
 // Function to distribute images into columns
